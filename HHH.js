2a1dcafd532a79b8e8418294f78bf0a337164594
Brett Wejrowski <brett@thewojogroup.com>
2012-03-01 16:33:05 -0500

    verbose small version

--- HHH.js
+++ HHH.js
@@ -1,3 +1,88 @@
-function v(h,d,f,g,p,m,w,z){e=8*m;a[k]=z;A(f,g+d*(75+e),35)(f,g+d*(30+e),55);a.fillText(w,f-100,g-50);a[k]=z.replace("d","9");D(q,f-40,g+d*(i+e),i,T)(q,f+i,g+d*(i+e),i,T);A(f-T,g+d*(91+e),5)(f+T,g+d*(91+e),5)(f-i,g+d*(50+e),T)(f+i,g+d*(50+e),T);m+=p;if(!m%15)p=0;else if(15<m)m=15;else if(0>m)m=0;else if(-1==p)for(;b=n[--l];)r(b.x-f)<i&&b.y<g+d*(e+10)&&b.y>g+d*(85+e)&&(1!=b&&w++,n[l]=1);l=0;return[h,d,f,g,p,m,w,z]}c.width=c.height=W=510;a.font="5ex y";
-var i=20,l=T=10,e,n=[],b,s=[0,-1,255,510,0,0,0,"#02d"],j=Math,x=2*j.PI,u=j.random,y=j.sqrt,r=j.abs,k="fillStyle",q="fillRect",o,t;onkeydown=onkeyup=function(h){32==h.which&&(s[4]=h.type[5]?1:-1)};for(A=function(h,d,f){D("beginPath")("arc",h,d,f,0,x,!0)("closePath")("fill");return A};-1<l--;)n.push({x:60+400*u(),y:60+400*u(),X:0,Y:0});D=function(h,d,f,g,p,m,w){a[h](d,f,g,p,m,w);return D};
-setInterval(function(){a[k]="#900";D(q,0,0,W,W);a[k]="#f00";for(A(255,255,250);b=n[++l];)if(1!=b){var h=b.x,d=b.y,h=r(h-255),d=r(d-255);o=y(h*h+d*d);t=40<o?-0.001125*o:0.016*o;b.x+=b.X+=0.5*t*(255<b.x?1:-1);b.y+=b.Y+=0.5*t*(255<b.y?1:-1);a[k]="#fff";A(b.x,b.y,i)}s=v.apply(c,s)},15);
+c.width = c.height = W = 510,
+a.font = "3em m",
+
+ 	z = 20, 
+	f = 255, 
+	g = n = k = u = v = 0, 
+	j = T = 11, 
+	l = [], 
+	o = [0, 0], 
+	p = Math, 
+	q = 2 * p.PI, 
+	r = p.random, 
+	s = p.sqrt, 
+	t = p.abs, 
+	u, 
+	v,
+
+// spacebar presses	
+onkeyup = onkeydown = function(d) {
+    32 == d.which && (o[0] = d.type[5] ? 1 : -1)
+},
+
+
+// draw a circle
+A = function(d, e, h, m) {
+	with(a)
+   		m && (a.fillStyle = "00fff00900".substr(m-1,3)),
+   		beginPath(), arc( d, e, h, 0, q, !0), fill();
+   	return A
+},
+
+// draw the hippo
+Z = function(d, e) {
+	
+	// movement offset
+    k = W - 8 * (e+=d);
+
+	// draw the body, eyes and nose
+    A(f, k - 75, 35, 1)(f, k - 30, 55)(f - T, k - 91, 5, 6)(f + T, k - 90, 5)(f - z, k - 50 , T)(f + z, k - 50, T);
+
+	// score
+    a.fillText(g, f - 100, W - 50);
+
+    // make sure the movement is within the bounds
+    !e % 15 ? d = 0 : 15 < e ? e = 15 : 0 > e && (e = 0); 
+
+	// iterate through the balls to see if we caught one
+	while (n=l[--j])
+        -1 == d && t(n.x - f) < z && n.y < k - 10 && n.y > k - 85 && (1 != n && g++, l[j] = 1);
+
+	// reset the iterator
+    j = 0;
+    return arguments
+};
+
+// create all the balls
+while(--j + 1)
+   	l.push({x: 60 + 400 * r(),y: 60 + 400 * r(),a: 0,b: 0});
+
+// main drawing function
+setInterval(function() {
+	
+	// draw the board & background
+    for (A(f, f, 2 * W, 8)(f, f, 250, 5 ); 
+
+	// iterate through the balls
+	n = l[++j]; )
+	
+		// check if its eaten
+        if (1 != n) {
+			
+			// get our distance from center 
+            var d = n.x, e = n.y, h = f, d = t(d - h), e = t(e - h), d = u = s(d * d + e * e);
+            
+			// get our acceleration based on position
+			v = 40 < d ? -0.001125 * d : 0.016 * d;
+			
+			// calculation the velocity and new position
+            n.x += n.a += 0.5 * v * (n.x > f ? 1 : -1);
+            n.y += n.b += 0.5 * v * (n.y > f ? 1 : -1);
+
+			// redraw the ball
+            A(n.x, n.y, z, 3)
+        }
+
+	// call the hippo draw function
+    o = Z.apply(c, o)
+}, 15);
